90654081ad0c5c73bd4f520d28d7b50c155fddc8
c20240025rayala-max/rayala <c20240025.rayala@csav.edu.ph>
2026-09-11 10:07:06 +0800

    Functions

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ console.log(`Name: ${myName}`);
 console.log(`Age: ${Age}`);
 console.log(`Number: ${number}`);
 console.log(`Address: ${address}`);
+
+//functions
+function greet(name) {
+    return`Good Morning, ${name}`;
+}
+console.log(greet('Rene'));
+
+//
+function mdas(num1,num2) {
+    let mul = num1 * num2;
+    let div = num1 / num2;
+    let add = num1 + num2;
+    let sub = num1 - num2;
+    return {mul, div, add, sub };
+}
+console.log (mdas(5, 3));   
+
+    
