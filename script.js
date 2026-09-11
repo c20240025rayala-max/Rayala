d30e4b2aae9aeb72752bf7072e630aae9f28c5c5
c20240025rayala-max/rayala <c20240025.rayala@csav.edu.ph>
2026-09-11 11:56:28 +0800

    .style

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
-console.log ("Hello World!");
-const myName = `Reden Rayala`;
-let Age = `20`;
-const number = 9123456789;
-const address = "Bacolod City, Negros Occidental, Philippines";
+console.log("Hello World!");
+
+const myName = 'Alken Flores';
+let Age = '20';
+const myNumber = 9638233541;
+const myAdress = 'Cadiz City';
+
 console.log(`Name: ${myName}`);
 console.log(`Age: ${Age}`);
-console.log(`Number: ${number}`);
-console.log(`Address: ${address}`);
+console.log(`Number: ${myNumber}`);
+console.log(`Adress: ${myAdress}`);
 
-//functions
+//Functions
 function greet(name) {
-    return`Good Morning, ${name}`;
+    return `Goodmorning, ${name}`;
 }
-console.log(greet('Rene'));
+console.log(greet(myName));
 
-//
-function mdas(num1,num2) {
-    let mul = num1 * num2;
-    let div = num1 / num2;
-    let add = num1 + num2;
-    let sub = num1 - num2;
-    return (`Value: ${num1}, ${num2}: { mul: ${mul}, div: ${div}, add: ${add}, sub: ${sub} }`);
+function mdas(num1, num2) {
+    let multiplacation = num1 * num2;
+    let division = num1 / num2;
+    let addition = num1 + num2;
+    let subtraction = num1 - num2;
+
+    return (`Values: ${num1} and ${num2}. Product: ${multiplacation}, Quotient: ${division}, Sum; ${addition},Diff: ${subtraction}`);
 }
-console.log (mdas(5, 3));
+console.log(mdas(5, 3))
+const heading = document.querySelector("h1")
+console.log(heading);
 
-//querySelector
-    const heading = document.querySelector("h1");
-    console.log(heading);
+const contactHeading = document.querySelector("#contact h2")
+console.log(contactHeading);
+const projectHeading = document.querySelector("#services h2")
+console.log(projectHeading);
 
-    const heading2 = document.querySelector("#contact h2");
-    console.log(heading2);
-
-    const heading3 = document.querySelector("#services h2");
-    console.log(heading3);
-
-//textContent
+const main = document.querySelectorAll("h2");
 heading.textContent = "My Portfolio";
-contactHeading.textContent = "let's connect";
-projectsHeading.textContent = "My Projects";
- 
+contactHeading.textContent = "Let's build together!"
+projectHeading.textContent = "Hello!";
 
-    
+heading.style.color = "purple";
+heading.style.backgroundColor = "yellow";
 
-
+//.style
+main[0].style.color = "purple";
+main[1].style.color = "purple";
+main[2].style.color = "purple";
